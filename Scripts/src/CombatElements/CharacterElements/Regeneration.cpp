adfacbb19d897a489d704704a245125fb650f535
Alias10294 <verzeleflorian@gmail.com>
2024-12-09 18:13:13 +0100

    Revert "Initial commit"
    This reverts commit e35a52b85018386ddbcb70672b391c50270ca9a6.

--- Scripts/src/CombatElements/CharacterElements/Regeneration.cpp
+++
@@ -1,5 +0,0 @@
-#include "../../../includes/CombatElements/CharacterElements/Regeneration.h"
-
-Regeneration::Regeneration(const std::int8_t amount, const float delay, const float rate)
-	: m_amount(amount), m_delay(delay), m_rate(rate)
-{ }
