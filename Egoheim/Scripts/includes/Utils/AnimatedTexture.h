1beeb813202fe06315c566b97cd9771b926a82eb
Alias10294 <verzeleflorian@gmail.com>
2024-12-15 23:09:49 +0100

    feat: added "reactive" mode for AnimatedTexture

--- Egoheim/Scripts/includes/Utils/AnimatedTexture.h
+++ Egoheim/Scripts/includes/Utils/AnimatedTexture.h
@@ -14,11 +14,13 @@ private:
 	SDL_Rect m_currentRect;
 	std::vector<float> m_frameTimes;
 	struct CurrentFrameInfo { int i; float time; } m_currentFrameInfo;
+	struct UpdateRythm { bool autonomous; int increment; } m_updateRythm;
 
 public:
-	AnimatedTexture(SDL_Texture* textures, const int nbFrames, const float frameTimes[]);
+	AnimatedTexture(SDL_Texture* textures, const int nbFrames, const float frameTimes[], bool autonomy = true);
 	~AnimatedTexture();
 
+	void StartAnimation();
 	void Update(float deltaTime);
 	void Render(SDL_Renderer* renderer, SDL_Rect* dstRect);
 };
--- Egoheim/Scripts/src/Utils/AnimatedTexture.cpp
+++ Egoheim/Scripts/src/Utils/AnimatedTexture.cpp
@@ -1,30 +1,46 @@
 #include "../../includes/Utils/AnimatedTexture.h"
 #include "../../includes/Game.h"
 
-AnimatedTexture::AnimatedTexture(SDL_Texture* textures, const int nbFrames, const float frameTimes[])
+AnimatedTexture::AnimatedTexture(SDL_Texture* textures, const int nbFrames, const float frameTimes[], bool autonomy)
 	: m_textures(textures), m_currentRect{ 0, 0, 0, 0 }, m_currentFrameInfo { 0, 0.0 }
 {
-
 	SDL_QueryTexture(textures, NULL, NULL, &m_currentRect.w, &m_currentRect.h);
 	m_currentRect.w /= nbFrames;
 
 	for (int i = 0; i < nbFrames; i++)
 		m_frameTimes.emplace_back(frameTimes[i]);
+
+	m_updateRythm = { autonomy, autonomy ? 1 : 0 };
 }
 AnimatedTexture::~AnimatedTexture()
 {
 	SDL_DestroyTexture(m_textures);
 }
 
+void AnimatedTexture::StartAnimation()
+{
+	if (!m_updateRythm.autonomous)
+		m_updateRythm.increment = m_currentFrameInfo.i == 0 ? 1 : -1;
+}
 void AnimatedTexture::Update(float deltaTime)
 {
 	m_currentFrameInfo.time += deltaTime;
 
 	while (m_currentFrameInfo.time >= m_frameTimes[m_currentFrameInfo.i])
 	{
-		m_currentFrameInfo.i = (m_currentFrameInfo.i + 1) % m_frameTimes.size();
-		m_currentFrameInfo.time -= m_frameTimes[m_currentFrameInfo.i];
-		m_currentRect.x = m_currentRect.w * m_currentFrameInfo.i;
+		if (m_updateRythm.increment != 0) 
+		{
+			m_currentFrameInfo.i = (m_currentFrameInfo.i + m_updateRythm.increment) % m_frameTimes.size();
+			m_currentFrameInfo.time -= m_frameTimes[m_currentFrameInfo.i];
+			m_currentRect.x = m_currentRect.w * m_currentFrameInfo.i;
+		}
+		if (!m_updateRythm.autonomous)
+		{
+			if (m_updateRythm.increment > 0 && m_currentFrameInfo.i == m_frameTimes.size())
+				m_updateRythm.increment = 0;
+			if (m_updateRythm.increment < 0 && m_currentFrameInfo.i == 0)
+				m_updateRythm.increment = 0;
+		}
 	}
 }
 void AnimatedTexture::Render(SDL_Renderer* renderer, SDL_Rect* dstRect)
