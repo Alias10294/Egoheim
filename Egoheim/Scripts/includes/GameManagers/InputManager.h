e5a76eb633a75e7103c67461e9824f10cdcc775b
Alias10294 <verzeleflorian@gmail.com>
2025-01-15 22:22:14 +0100

    feat: emptied InputManager for full refactor

--- Egoheim/Scripts/includes/GameManagers/InputManager.h
+++ Egoheim/Scripts/includes/GameManagers/InputManager.h
@@ -5,10 +5,6 @@
 #include <vector>
 #include <unordered_map>
 
-constexpr const int NB_SCANCODES = SDL_NUM_SCANCODES;
-constexpr const int NB_MOUSE_BUTTONS = 5;
-constexpr const int NB_CONTROLLERS = 2;
-
 class InputManager 
 {
 public:
@@ -18,56 +14,7 @@ public:
     void Init();
     void Quit();
 
-    void HandleEvent(SDL_Event& event);
-    void UpdatePreviousStates();
-
-    bool IsKeyPressed(SDL_Scancode scancode) const;
-    bool IsKeyJustPressed(SDL_Scancode scancode) const;
-    bool IsKeyJustReleased(SDL_Scancode scancode) const;
-
-    bool IsMouseButtonPressed(int button) const;
-    bool IsMouseButtonJustPressed(int button) const;
-    bool IsMouseButtonJustReleased(int button) const;
-    int GetMouseX() const;
-    int GetMouseY() const;
-
-    bool IsControllerButtonPressed(int controllerIndex, SDL_GameControllerButton button) const;
-    bool IsControllerButtonJustPressed(int controllerIndex, SDL_GameControllerButton button) const;
-    bool IsControllerButtonJustReleased(int controllerIndex, SDL_GameControllerButton button) const;
-    int GetControllerAxis(int controllerIndex, SDL_GameControllerAxis axis) const;
-
-
-private:
-    void HandleKeyboardEvent(const SDL_KeyboardEvent& keyboardEvent);
-    void HandleMouseMotionEvent(const SDL_MouseMotionEvent& mouseMotionEvent);
-    void HandleMouseButtonEvent(const SDL_MouseButtonEvent& mouseButtonEvent);
-    void HandleMouseWheelEvent(const SDL_MouseWheelEvent& mouseWheelEvent);
-    void HandleControllerDeviceAdded(int deviceIndex);
-    void HandleControllerDeviceRemoved(SDL_JoystickID joystickID);
-    void HandleControllerButtonEvent(const SDL_ControllerButtonEvent& cbuttonEvent);
-    void HandleControllerAxisEvent(const SDL_ControllerAxisEvent& caxisEvent);
-
-    bool m_currentKeyStates[NB_SCANCODES];
-    bool m_previousKeyStates[NB_SCANCODES];
-
-    bool m_currentMouseButtonStates[NB_MOUSE_BUTTONS];
-    bool m_previousMouseButtonStates[NB_MOUSE_BUTTONS];
-    int m_mouseX;
-    int m_mouseY;
-    int m_mouseWheelDelta;
-
-
-    struct ControllerInput
-    {
-        SDL_GameController* controller = nullptr;
-        SDL_JoystickID joystickID = -1;
-
-        std::unordered_map<SDL_GameControllerButton, bool> currentButtonStates;
-        std::unordered_map<SDL_GameControllerButton, bool> previousButtonStates;
-
-        std::unordered_map<SDL_GameControllerAxis, int> axisValues;
-    };
-    ControllerInput m_controllerInput[NB_CONTROLLERS];
+    // TODO ALL METHODS
 };
 
 #endif INPUTMANAGER_H
